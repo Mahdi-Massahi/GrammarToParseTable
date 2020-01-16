52f49652c2a201b687eaf0cb91e4e09594d14963
Mahdi-Massahi <Mahdi.massahi@gmail.com>
2020-01-16 17:52:51 +0330

    DataGrid Theme modefing

---
+++ GrammarToParseTable/Grammer/Symbol.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrammarToParseTable.Classes
+{
+    public class Symbol
+    {
+        public char character {private set; get;}
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ch">A character as a variable</param>
+        public Symbol(char ch)
+        {
+            character = ch;
+        }
+    }
+}
