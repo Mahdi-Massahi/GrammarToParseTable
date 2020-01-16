6419435a8905b92216511bc76c51f5cae9d8b56d
MohammadSalek <salek.mohmd@gmail.com>
2020-01-16 21:25:58 +0330

    Created ParseTable

--- GrammarToParseTable/Grammer/Follow.cs
+++ GrammarToParseTable/Grammer/Follow.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GrammarToParseTable.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace GrammarToParseTable.Grammer
 {
     class Follow
     {
+        public static Symbol FindFollow(List<Rule> rules, Dictionary<Rule, Symbol> firsts, Rule rule)
+        {
+            return null; //TODO
+        }       
     }
 }
